b6c594c7711a8dc253aac4fdc7cf2e38a3c31de5
Baptiste Florentin <baptiste.florentin.pro@gmail.com>
2025-07-06 08:10:02 +0200

    fix: bad layout

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -5,28 +5,61 @@ import { Toaster } from "@/components/ui/toaster";
 import { Flex, Text, Heading, Container, Box } from "@radix-ui/themes";
 import { Suspense } from "react";
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: "Self Workshop",
+  description: "Self Workshop",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
-    <Suspense>
-      <Theme accentColor="teal" grayColor="slate" radius="large" scaling="100%">
-        <ToastProvider>
-          <Container size="3" p="6">
-            <Flex direction="column" gap="6" align="center">
-              {/* Header */}
-              <Box style={{ textAlign: "center" }}>
-                <Heading size="8" mb="2">
-                  BackupBuddy
-                </Heading>
-                <Text size="5" color="gray">
-                  Social Recovery 4 Everyone
-                </Text>
-              </Box>
-              {children}
-            </Flex>
-          </Container>
-          <Toaster />
-        </ToastProvider>
-      </Theme>
-    </Suspense>
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <Suspense>
+          <Theme
+            accentColor="teal"
+            grayColor="slate"
+            radius="large"
+            scaling="100%">
+            <ToastProvider>
+              <Container size="3" p="6">
+                <Flex direction="column" gap="6" align="center">
+                  {/* Header */}
+                  <Box style={{ textAlign: "center" }}>
+                    <Heading size="8" mb="2">
+                      BackupBuddy
+                    </Heading>
+                    <Text size="5" color="gray">
+                      Social Recovery 4 Everyone
+                    </Text>
+                  </Box>
+                  {children}
+                </Flex>
+              </Container>
+              <Toaster />
+            </ToastProvider>
+          </Theme>
+        </Suspense>
+      </body>
+    </html>
   );
 }
